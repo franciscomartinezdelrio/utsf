ec8b0c05f62238ac88776507c947c0cca107ad32
franciscomartinezdelrio <fmartin@ujaen.es>
2025-10-17 11:16:45 +0200

    Irrelevant

--- R/recursive_prediction.R
+++ R/recursive_prediction.R
@@ -5,11 +5,11 @@ recursive_prediction <- function(object, h) {
   prediction <- numeric(h)
   values <- as.vector(ts)
   # tiempos <- stats::time(stats::ts(1:(length(ts)+h), start = start(ts), frequency = frequency(ts))) #añadido
-  # ciclo <- stats::cycle(stats::ts(1:(length(ts)+h), start = start(ts), frequency = frequency(ts))) #añadido
+  # ciclo <- stats::cycle(stats::ts(1:(length(ts)+h), start = start(ts), frequency = frequency(ts))) # Quitar
   for (hor in 1:h) {
     example <- values[(length(values) + 1) - rev(object$lags)]
     #example[length(example) + 1] <- tiempos[length(values) + 1 - rev(object$lags)[1]] # añadido
-    #example[length(example) + 1] <- ciclo[length(ts)+hor]
+    # example[length(example) + 1] <- ciclo[length(ts)+hor] # Quitar
     prediction[hor] <- predict_one_value_transforming(object, example)
     values <- c(values, prediction[hor])
   }
